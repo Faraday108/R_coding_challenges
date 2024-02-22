fe681d5c1eb08566d8cbdae846e3c92c528f4424
Nathan Young <youngnt225@gmail.com>
2024-02-22 09:08:19 -0500

    Completed Challenge 1
    Implemented mean_median function that takes a vector and returns a list of the vector's mean and median without calling `mean` and `median`

---
+++ Challenge1_mean_median.R
@@ -0,0 +1,24 @@
+## Coding challenge 1
+# Prompt from ChatGPT: 
+
+# Task: Write an R function that takes a numeric vector as input and returns 
+# the mean and median of the vector.
+
+# Constraints:
+  
+# The function should be named mean_median.
+# It should take a single argument, x, which is a numeric vector.
+# It should return a named list with two elements: mean and median.
+
+mean_median <- function(x) {
+  if(class(x) != "numeric") {
+    stop("Please enter a numeric vector")
+  }
+  len <- length(x)
+  out <- list(mean = numeric(), median = numeric())
+  out$mean <- sum(x)/len
+  out$median <- ifelse(len %% 2 == 1, # test if odd
+                       x[len %/% 2 + 1], # if T, return middle
+                       sum(x[(len %/% 2):(len %/% 2 + 1)])/2)
+  out
+}
