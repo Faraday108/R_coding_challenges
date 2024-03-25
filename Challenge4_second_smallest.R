34f70f9ead965c98aa8a7dcf9e291b3db9007935
Nathan Young <youngnt225@gmail.com>
2024-03-24 21:58:38 -0400

    Update Challenge4_second_smallest.R

--- Challenge4_second_smallest.R
+++ Challenge4_second_smallest.R
@@ -9,7 +9,19 @@
 # The function should return the second smallest number in the list.
 
 second_smallest <- function(lst) {
-  lst <- unlist(lst)
-  rnk <- rank(lst)
-  lst[rnk == 2]
+  islist <- is.list(lst) # error condition 1
+  isnumeric <- all(sapply(lst, is.numeric)) # error condition 2
+  if(!islist) {# ensure a list was entered
+    stop("Enter a list of numeric integers")
+  } else if(!isnumeric){ # ensure that arguments are numeric
+    stop("Check all elements of list are numeric")
+  }
+  
+  if(length(lst) > 1){
+    lst <- unlist(lst)
+    rnk <- rank(lst)
+    lst[rnk == 2]
+  } else {
+    return(NULL)
+  }
 }
